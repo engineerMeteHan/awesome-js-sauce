d9505d404d42927392e23df36c3cb84808bd3777
Metehan GENCER <metehangencer@gmail.com>
2024-08-11 20:10:51 +0300

    improved awesome js

--- src/awesome.js
+++ src/awesome.js
@@ -3,5 +3,6 @@ export default {
     alert('your crap website is now been awesome sauced');
     console.log('your crap website is now awesome');
     console.log('Hello world');
+    console.log("Metehan GENCER is new added as folder");
   }
 }
