f001c725d05ec629a69b1a5d15914328b82a7799
metehan gencer <metehangencereem@gmail.com>
2024-08-12 14:30:18 +0300

    Added new feature now

--- src/awesome.js
+++ src/awesome.js
@@ -4,5 +4,6 @@ export default {
     console.log('your crap website is now awesome');
     console.log('Hello world');
     console.log("Metehan GENCER is new added as folder");
+    console.log("Hello this is Metehan GENCER. I love Coding so I love World");
   }
 }
